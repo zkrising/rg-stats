a3b3b7f0af7296c3097d6129f7a846a60aa86824
zkldi <20380519+zkldi@users.noreply.github.com>
2022-01-24 18:53:00 +0000

    Snapshot

---
+++ tap-snapshots/src/algorithms/chunithm-rating.test.ts.test.cjs
@@ -0,0 +1,18 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`src/algorithms/chunithm-rating.test.ts TAP CHUNITHM Rating Validation Tests > Should throw if chart level is negative. 1`] = `
+Invalid input, Chart level cannot be negative. internalChartLevel=-1.
+`
+
+exports[`src/algorithms/chunithm-rating.test.ts TAP CHUNITHM Rating Validation Tests > Should throw if your score is >= 1.01million. 1`] = `
+Invalid input, Score cannot be greater than 1.01Million. score=1010001.
+`
+
+exports[`src/algorithms/chunithm-rating.test.ts TAP CHUNITHM Rating Validation Tests > Should throw if your score is negative. 1`] = `
+Invalid input, Score cannot be negative. score=-1.
+`
