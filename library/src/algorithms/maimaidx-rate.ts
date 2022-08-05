f58194d47622165c13f7feff8729158644c72234
zkldi <20380519+zkldi@users.noreply.github.com>
2022-08-06 00:18:31 +0100

    style: run linter

--- library/src/algorithms/maimaidx-rate.ts
+++ library/src/algorithms/maimaidx-rate.ts
@@ -4,14 +4,11 @@ import { ThrowIf } from "../util/throw-if";
 /**
  * Calculate maimai DX rate for a score.
  *
- * @param actualScore - The score to calculate the rate for.
+ * @param score - The score to calculate the rate for.
  * @param internalChartLevel - The internal decimal level of the chart the score was achieved on.
  */
 
-export function calculate(actualScore: number, internalChartLevel: number) {
-	// reassignment so ESLinter doesn't cry about it
-	let score = actualScore;
-
+export function calculate(score: number, internalChartLevel: number) {
 	ThrowIf(score > 101, "Score cannot be greater than 101%.", { score });
 	ThrowIf.negative(score, "Score cannot be negative.", { score });
 	ThrowIf.negative(internalChartLevel, "Internal chart level cannot be negative.", {
@@ -19,40 +16,40 @@ export function calculate(actualScore: number, internalChartLevel: number) {
 	});
 
 	// minimum score coefficient is 5
-	let scoreCoeff = 5;
+	let scoreCoef = 5;
 
 	// score coefficient number set obtained from maimai bookmarklet code.
-        // scores above 100.5% are considered 100.5% by the algorithm.
+	// scores above 100.5% are considered 100.5% by the algorithm.
 	if (score >= 100.5) {
 		score = 100.5;
-		scoreCoeff = 22.4;
+		scoreCoef = 22.4;
 	} else if (score >= 100) {
-		scoreCoeff = 21.6;
+		scoreCoef = 21.6;
 	} else if (score >= 99.5) {
-		scoreCoeff = 21.1;
+		scoreCoef = 21.1;
 	} else if (score >= 99) {
-		scoreCoeff = 20.8;
+		scoreCoef = 20.8;
 	} else if (score >= 98) {
-		scoreCoeff = 20.3;
+		scoreCoef = 20.3;
 	} else if (score >= 97) {
-		scoreCoeff = 20;
+		scoreCoef = 20;
 	} else if (score >= 94) {
-		scoreCoeff = 16.8;
+		scoreCoef = 16.8;
 	} else if (score >= 90) {
-		scoreCoeff = 15.2;
+		scoreCoef = 15.2;
 	} else if (score >= 80) {
-		scoreCoeff = 13.6;
+		scoreCoef = 13.6;
 	} else if (score >= 75) {
-		scoreCoeff = 12;
+		scoreCoef = 12;
 	} else if (score >= 70) {
-		scoreCoeff = 11.2;
+		scoreCoef = 11.2;
 	} else if (score >= 60) {
-		scoreCoeff = 9.6;
+		scoreCoef = 9.6;
 	} else if (score >= 50) {
-		scoreCoeff = 8;
+		scoreCoef = 8;
 	}
 
-	return FloorToNDP(scoreCoeff * internalChartLevel * (score / 100), 0);
+	return FloorToNDP(scoreCoef * internalChartLevel * (score / 100), 0);
 }
 
 /**
