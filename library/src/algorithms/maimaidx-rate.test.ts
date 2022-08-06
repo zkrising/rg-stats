4ce2b7fba0d1bb5b660564e5ac4556ce7dbcf2ff
zkldi <20380519+zkldi@users.noreply.github.com>
2022-08-06 13:14:49 +0100

    fix: remove inverse as it's not calculable

--- library/src/algorithms/maimaidx-rate.test.ts
+++ library/src/algorithms/maimaidx-rate.test.ts
@@ -2,7 +2,7 @@ import t from "tap";
 import { isAprx } from "../test-utils/approx";
 import { TestCase } from "../test-utils/test-case";
 import { ThrowsToSnapshot } from "../test-utils/throw-snapshot";
-import { calculate, inverse } from "./maimaidx-rate";
+import { calculate } from "./maimaidx-rate";
 
 t.test("maimai DX Rate Tests", (t) => {
 	function MakeTestCase(score: number, level: number, expectedRate: number): TestCase {
@@ -58,50 +58,6 @@ t.test("maimai DX Rate Validation Tests", (t) => {
 	ThrowsToSnapshot(t, () => calculate(-1, 10), "Should throw if score is negative.");
 	ThrowsToSnapshot(t, () => calculate(101.5, 10), "Should throw if score is greater than 101%.");
 	ThrowsToSnapshot(t, () => calculate(99.5, -1), "Should throw if level is negative.");
-
-	t.end();
-});
-
-t.test("maimai DX Inverse Rate Tests", (t) => {
-	function MakeTestCase(expectedScore: number, level: number, rate: number): TestCase {
-		return (t) =>
-			isAprx(
-				t,
-				inverse(rate, level),
-				expectedScore,
-				`A rate of ${rate} on a chart of level ${level} should invert to ${expectedScore} rate.`
-			);
-	}
-
-	// Assertions all copied from the previous tests, and just applied in reverse.
-	// (and floored to the nearest boundary, because that's how it works.)
-	const testCases = [
-		MakeTestCase(100.5, 13, 292),
-		MakeTestCase(100, 13, 280),
-		MakeTestCase(99.5, 13.7, 287),
-		MakeTestCase(99, 12.7, 261),
-		MakeTestCase(98, 10, 198),
-		MakeTestCase(97, 8, 155),
-		MakeTestCase(94, 10.7, 168),
-		MakeTestCase(90, 5, 68),
-		MakeTestCase(80, 11.5, 125),
-		MakeTestCase(75, 12.4, 111),
-		MakeTestCase(70, 14.2, 111),
-		MakeTestCase(60, 15, 86),
-		MakeTestCase(50, 12.6, 50),
-		MakeTestCase(12, 12.6, 7),
-	];
-
-	for (const testCase of testCases) {
-		testCase(t);
-	}
-
-	t.end();
-});
-
-t.test("maimai DX Inverse Rate Validation Tests", (t) => {
-	ThrowsToSnapshot(t, () => inverse(50, -1), "Should throw if level is negative.");
-	ThrowsToSnapshot(t, () => inverse(100, 1), "Should throw if rate is impossible.");
 
 	t.end();
 });
--- library/src/algorithms/maimaidx-rate.ts
+++ library/src/algorithms/maimaidx-rate.ts
@@ -1,4 +1,4 @@
-import { RoundToNDP, FloorToNDP } from "../util/math";
+import { FloorToNDP } from "../util/math";
 import { ThrowIf } from "../util/throw-if";
 
 /**
@@ -50,62 +50,4 @@ export function calculate(score: number, internalChartLevel: number) {
 	}
 
 	return FloorToNDP(scoreCoef * internalChartLevel * (score / 100), 0);
-}
-
-/**
- * Given a maimai DX rate, a chart level, return the minimum score necessary to get
- * that rate.
- *
- * @param rate - The rate to inverse.
- * @param internalChartLevel - The internal decimal level of the chart the rate was on.
- */
-
-export function inverse(rate: number, internalChartLevel: number) {
-	ThrowIf.negative(internalChartLevel, "Internal chart level cannot be negative.", {
-		level: internalChartLevel,
-	});
-
-	// rounded to 4dp, purely because mathematically there should only be 4dp after calculating.
-	const scoreFactor = RoundToNDP(rate / internalChartLevel, 4);
-
-	ThrowIf(
-		scoreFactor > 22.4 * 1.005,
-		`A rate of ${rate} is not possible on a chart of level ${internalChartLevel}.`,
-		{
-			rate,
-			level: internalChartLevel,
-		}
-	);
-
-	// shoutout to my friend Peachan who un-screwed this for me.
-	// my brain wouldn't have got here without them.
-	if (scoreFactor > 21.6 * 1) {
-		return 100.5;
-	} else if (scoreFactor > 21.1 * 0.995) {
-		return 100;
-	} else if (scoreFactor > 20.8 * 0.99) {
-		return 99.5;
-	} else if (scoreFactor > 20.3 * 0.98) {
-		return 99;
-	} else if (scoreFactor > 20 * 0.97) {
-		return 98;
-	} else if (scoreFactor > 16.8 * 0.94) {
-		return 97;
-	} else if (scoreFactor > 15.2 * 0.9) {
-		return 94;
-	} else if (scoreFactor > 13.6 * 0.8) {
-		return 90;
-	} else if (scoreFactor > 12 * 0.75) {
-		return 80;
-	} else if (scoreFactor > 11.2 * 0.7) {
-		return 75;
-	} else if (scoreFactor > 9.6 * 0.6) {
-		return 70;
-	} else if (scoreFactor > 8 * 0.5) {
-		return 60;
-	} else if (scoreFactor > 0) {
-		return 50;
-	}
-
-	return 0;
 }
