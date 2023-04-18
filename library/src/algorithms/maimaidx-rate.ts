d870b5915e35fb8b24c3bb8b92b7daa78da16326
beerpiss <lacvtg.a1.2023@gmail.com>
2023-04-18 15:34:40 +0700

    [maimaidx] add extra coefficients

--- library/src/algorithms/maimaidx-rate.ts
+++ library/src/algorithms/maimaidx-rate.ts
@@ -1,8 +1,34 @@
 import { FloorToNDP } from "../util/math";
 import { ThrowIf } from "../util/throw-if";
 
+const RATING_COEFFICIENTS = new Map([
+	[100.5, 22.4],
+	[100.4999, 22.2],
+	[100, 21.6],
+	[99.9999, 21.4],
+	[99.5, 21.1],
+	[99, 20.8],
+	[98.9999, 20.6],
+	[98, 20.3],
+	[97, 20],
+	[96.9999, 17.6],
+	[94, 16.8],
+	[90, 15.2],
+	[80, 13.6],
+	[79.9999, 12.8],
+	[75, 12],
+	[70, 11.2],
+	[60, 9.6],
+	[50, 8],
+	[40, 6.4],
+	[30, 4.8],
+	[20, 3.2],
+	[10, 1.6],
+	[0, 0],
+]);
+
 /**
- * Calculate maimai DX rate for a score.
+ * Calculate maimai DX Splash+ (and newer) rate for a score.
  *
  * @param score - The score to calculate the rate for.
  * @param internalChartLevel - The internal decimal level of the chart the score was achieved on.
@@ -14,39 +40,14 @@ export function calculate(score: number, internalChartLevel: number) {
 		level: internalChartLevel,
 	});
 
-	// minimum score coefficient is 5
-	let scoreCoef = 5;
+	// Scores above SSS+ are capped at 100.5%
+	score = Math.min(score, 100.5);
 
-	// score coefficient number set obtained from maimai bookmarklet code.
-	// scores above 100.5% are considered 100.5% by the algorithm.
-	if (score >= 100.5) {
-		score = 100.5;
-		scoreCoef = 22.4;
-	} else if (score >= 100) {
-		scoreCoef = 21.6;
-	} else if (score >= 99.5) {
-		scoreCoef = 21.1;
-	} else if (score >= 99) {
-		scoreCoef = 20.8;
-	} else if (score >= 98) {
-		scoreCoef = 20.3;
-	} else if (score >= 97) {
-		scoreCoef = 20;
-	} else if (score >= 94) {
-		scoreCoef = 16.8;
-	} else if (score >= 90) {
-		scoreCoef = 15.2;
-	} else if (score >= 80) {
-		scoreCoef = 13.6;
-	} else if (score >= 75) {
-		scoreCoef = 12;
-	} else if (score >= 70) {
-		scoreCoef = 11.2;
-	} else if (score >= 60) {
-		scoreCoef = 9.6;
-	} else if (score >= 50) {
-		scoreCoef = 8;
+	for (const [scoreBoundary, coefficient] of RATING_COEFFICIENTS) {
+		if (score >= scoreBoundary) {
+			return FloorToNDP(internalChartLevel * coefficient * (score / 100), 0);
+		}
 	}
 
-	return FloorToNDP(scoreCoef * internalChartLevel * (score / 100), 0);
+	return 0;
 }
