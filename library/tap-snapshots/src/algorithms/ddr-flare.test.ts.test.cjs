ebaca4bee04f646b3b14cbc64ac827cea45645d8
Gyoo <gaetan.young@gmail.com>
2024-07-27 13:17:13 +0200

    feat: DDR rating

---
+++ library/tap-snapshots/src/algorithms/ddr-flare.test.ts.test.cjs
@@ -0,0 +1,22 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`src/algorithms/ddr-flare.test.ts TAP DDR Flare Validation Tests > Should throw if chart level is greater than 19. 1`] = `
+Invalid input, Internal chart level cannot be greater than 19. level=20.
+`
+
+exports[`src/algorithms/ddr-flare.test.ts TAP DDR Flare Validation Tests > Should throw if chart level is negative. 1`] = `
+Invalid input, Internal chart level cannot be negative or zero. level=-1.
+`
+
+exports[`src/algorithms/ddr-flare.test.ts TAP DDR Flare Validation Tests > Should throw if flare level is greater than 10. 1`] = `
+Invalid input, Flare level cannot be greater than 10. level=11.
+`
+
+exports[`src/algorithms/ddr-flare.test.ts TAP DDR Flare Validation Tests > Should throw if flare level is negative. 1`] = `
+Invalid input, Flare level cannot be negative. level=-1.
+`
