504f952d19f72f000738150af04409cf719ab442
beer-psi <lacvtg.a1.2023@gmail.com>
2024-08-25 12:12:27 +0700

    [maimai, maimaidx, arcaea] switch calculation to fixed-point arithmetic

--- library/src/algorithms/maimai-rate.ts
+++ library/src/algorithms/maimai-rate.ts
@@ -1,42 +1,41 @@
-import { FloorToNDP } from "../util/math";
 import { ThrowIf } from "../util/throw-if";
 
 // [F, E, D, C, B, A, AA, AAA, S - 0.01, S, SS - 0.01, SS, SSS - 0.01, SSS, SSS+]
 const RATE_TABLE: Record<number, number[]> = {
 	0: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	1: [0, 0, 0.1, 0.2, 0.4, 1, 1.5, 2.5, 4, 4, 5, 5.5, 6.5, 7, 8],
-	2: [0, 0, 0.2, 0.4, 0.8, 2, 2.5, 3, 4.5, 4.5, 5.5, 6, 7, 7.5, 8.5],
-	3: [0, 0, 0.3, 0.6, 1.2, 3, 3.5, 4, 5, 5, 6, 6.5, 7.5, 8, 9],
-	4: [0, 0, 0.4, 0.8, 1.6, 3.5, 4, 4.5, 5.5, 5.5, 6.5, 7, 8, 8.5, 9.5],
-	5: [0, 0, 0.5, 1, 2, 4, 5, 5.5, 6, 6, 7, 7.5, 8.5, 9, 10],
-	6: [0, 0, 0.6, 1.2, 2.4, 4.5, 5.5, 6, 6.5, 6.5, 7.5, 8, 9, 9.5, 10.5],
-	7: [0, 0, 0.7, 1.4, 2.8, 5, 6, 7, 7.25, 7.5, 8.75, 9, 9.75, 10, 11],
-	8: [0, 0, 0.8, 1.6, 3.2, 5.5, 6.5, 7.25, 7.5, 8, 9.25, 9.5, 10.25, 10.5, 11.5],
-	9: [0, 0, 0.9, 1.8, 3.6, 6, 7, 7.5, 8, 9, 10.25, 10.5, 11.25, 11.5, 12.5],
-	10: [0, 0, 1, 2, 4, 7, 8, 8.5, 9, 10, 11.25, 11.5, 12.25, 12.5, 13.5],
-	11: [0, 0, 1.1, 2.2, 4.4, 8, 9, 9.5, 10, 11, 11.75, 12, 12.75, 13, 14],
-	12: [0, 0, 1.2, 2.4, 4.8, 9, 10, 10.5, 11, 12, 12.75, 13, 13.75, 14, 15],
-	13: [0, 0, 1.3, 2.6, 5.2, 10, 11, 11.5, 12.5, 13.5, 14.75, 15, 15.75, 16, 17],
-	14: [0, 0, 1.4, 2.8, 5.6, 11, 12, 12.5, 14, 15, 16.75, 17, 17.75, 18, 19],
-	15: [0, 0, 1.5, 3, 6, 12, 13, 13.5, 15.5, 16.5, 18.75, 19, 19.75, 20, 21],
+	10: [0, 0, 10, 20, 40, 100, 150, 250, 400, 400, 500, 550, 650, 700, 800],
+	20: [0, 0, 20, 40, 80, 200, 250, 300, 450, 450, 550, 600, 700, 750, 850],
+	30: [0, 0, 30, 60, 120, 300, 350, 400, 500, 500, 600, 650, 750, 800, 900],
+	40: [0, 0, 40, 80, 160, 350, 400, 450, 550, 550, 650, 700, 800, 850, 950],
+	50: [0, 0, 50, 100, 200, 400, 500, 550, 600, 600, 700, 750, 850, 900, 1000],
+	60: [0, 0, 60, 120, 240, 450, 550, 600, 650, 650, 750, 800, 900, 950, 1050],
+	70: [0, 0, 70, 140, 280, 500, 600, 700, 725, 750, 875, 900, 975, 1000, 1100],
+	80: [0, 0, 80, 160, 320, 550, 650, 725, 750, 800, 925, 950, 1025, 1050, 1150],
+	90: [0, 0, 90, 180, 360, 600, 700, 750, 800, 900, 1025, 1050, 1125, 1150, 1250],
+	100: [0, 0, 100, 200, 400, 700, 800, 850, 900, 1000, 1125, 1150, 1225, 1250, 1350],
+	110: [0, 0, 110, 220, 440, 800, 900, 950, 1000, 1100, 1175, 1200, 1275, 1300, 1400],
+	120: [0, 0, 120, 240, 480, 900, 1000, 1050, 1100, 1200, 1275, 1300, 1375, 1400, 1500],
+	130: [0, 0, 130, 260, 520, 1000, 1100, 1150, 1250, 1350, 1475, 1500, 1575, 1600, 1700],
+	140: [0, 0, 140, 280, 560, 1100, 1200, 1250, 1400, 1500, 1675, 1700, 1775, 1800, 1900],
+	150: [0, 0, 150, 300, 600, 1200, 1300, 1350, 1550, 1650, 1875, 1900, 1975, 2000, 2100],
 };
 
 const RANK_BOUNDARIES: Record<string, number> = {
 	F: 0,
-	E: 10,
-	D: 20,
-	C: 40,
-	B: 60,
-	A: 80,
-	AA: 90,
-	AAA: 94,
-	"S-": 96.99,
-	S: 97,
-	"SS-": 98.99,
-	SS: 99,
-	"SSS-": 99.99,
-	SSS: 100,
-	"SSS+": 104,
+	E: 1000,
+	D: 2000,
+	C: 4000,
+	B: 6000,
+	A: 8000,
+	AA: 9000,
+	AAA: 9400,
+	"S-": 9699,
+	S: 9700,
+	"SS-": 9899,
+	SS: 9900,
+	"SSS-": 9999,
+	SSS: 10000,
+	"SSS+": 10400,
 };
 
 // @ts-expect-error getRank is called after the score is checked for validity
@@ -49,26 +48,27 @@ function getRank(score: number): keyof typeof RANK_BOUNDARIES {
 	}
 }
 
-function calculateCurve(internalChartLevel: number): number[] {
-	let lower = Math.floor(internalChartLevel);
-	let upper = Math.ceil(internalChartLevel);
+function calculateCurve(iclInt: number): number[] {
+	let lowerIcl = Math.floor(iclInt / 10) * 10;
+	let upperIcl = Math.ceil(iclInt / 10) * 10;
 
 	// extrapolate for levels beyond 15
-	if (internalChartLevel > 15) {
-		lower = 14;
-		upper = 15;
+	if (iclInt > 150) {
+		lowerIcl = 140;
+		upperIcl = 150;
 	}
 
-	if (lower === upper) {
-		return RATE_TABLE[lower];
+	if (lowerIcl === upperIcl) {
+		return RATE_TABLE[lowerIcl];
 	}
 
-	const lowerCurve = RATE_TABLE[lower];
-	const upperCurve = RATE_TABLE[upper];
+	const lowerCurve = RATE_TABLE[lowerIcl];
+	const upperCurve = RATE_TABLE[upperIcl];
 
 	return lowerCurve.map((value, index) => {
 		const upperValue = upperCurve[index];
-		return value + (upperValue - value) * (internalChartLevel - lower);
+
+		return value + ((upperValue - value) * (iclInt - lowerIcl)) / (upperIcl - lowerIcl);
 	});
 }
 
@@ -89,16 +89,21 @@ export function calculate(score: number, maxScore: number, internalChartLevel: n
 		level: internalChartLevel,
 	});
 
-	if (score <= 10) {
+	const scoreInt = Math.round(score * 100);
+	const maxScoreInt = Math.round(maxScore * 100);
+	const iclInt = Math.round(internalChartLevel * 10);
+
+	if (scoreInt <= 1000) {
 		return 0;
 	}
 
-	const curve = calculateCurve(internalChartLevel);
-	if (score === maxScore) {
-		return curve[curve.length - 1];
+	const curve = calculateCurve(iclInt);
+
+	if (scoreInt === maxScoreInt) {
+		return curve[curve.length - 1] / 100;
 	}
 
-	const rank = getRank(score);
+	const rank = getRank(scoreInt);
 	const rankIndex = Object.keys(RANK_BOUNDARIES).indexOf(rank);
 
 	const lower = curve[rankIndex];
@@ -106,7 +111,7 @@ export function calculate(score: number, maxScore: number, internalChartLevel: n
 	const lowerBoundary = RANK_BOUNDARIES[rank];
 	const upperBoundary = Object.values(RANK_BOUNDARIES)[rankIndex + 1];
 	const rate =
-		lower + (upper - lower) * ((score - lowerBoundary) / (upperBoundary - lowerBoundary));
+		lower + (upper - lower) * ((scoreInt - lowerBoundary) / (upperBoundary - lowerBoundary));
 
-	return FloorToNDP(rate, 2);
+	return rate / 100;
 }
